e7a021e5736c1245a3d93fdaeba971f852f97767
Michael Whatcott <mdwhatcott@gmail.com>
2017-09-08 17:12:50 -0600

    Working toward completing international-street-api

---
+++ cmd/international-street-api/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/url"
+
+	cli "bitbucket.org/michael-whatcott/smarty-cli"
+	"github.com/mdwhatcott/helps"
+	"github.com/smartystreets/smartystreets-go-sdk/international-street-api"
+	"github.com/smartystreets/smartystreets-go-sdk/wireup"
+)
+
+func main() {
+	log.SetFlags(log.Lmicroseconds)
+
+	inputs := NewInputs()
+	builder := wireup.NewClientBuilder().WithSecretKeyCredential(inputs.AuthID, inputs.AuthToken).WithDebugHTTPOutput()
+	client := builder.BuildInternationalStreetAPIClient()
+	lookup := inputs.AssembleLookup()
+
+	if err := client.SendLookup(lookup); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Formatted Result:")
+	fmt.Println(helps.DumpJSON(lookup.Results))
+}
+
+///////////////////
+
+type Inputs struct {
+	*cli.Inputs
+
+	country            string
+	language           string
+	freeform           string
+	address1           string
+	address2           string
+	address3           string
+	address4           string
+	organization       string
+	locality           string
+	administrativeArea string
+	postalCode         string
+	geocode            bool
+
+	lookup *street.Lookup
+}
+
+func NewInputs() *Inputs {
+	this := &Inputs{
+		Inputs: cli.NewInputs(),
+		lookup: new(street.Lookup),
+	}
+	this.flags()
+	return this
+}
+
+func (this *Inputs) flags() {
+	// TODO
+	flag.Parse()
+}
+
+func (this *Inputs) AssembleLookup() *street.Lookup {
+	values, _ := url.ParseQuery(this.RawQuery)
+	this.assembleLookupFromQueryString(values)
+	if this.lookup.Freeform != "" || this.lookup.Address1 != "" {
+		return this.lookup
+	}
+
+	if address, _ := url.Parse(this.RawURL); address != nil {
+		this.assembleLookupFromQueryString(address.Query())
+	}
+	if this.lookup.Freeform != "" || this.lookup.Address1 != "" {
+		return this.lookup
+	}
+
+	this.assembleLookupFromFlags()
+
+	if this.lookup.Freeform != "" || this.lookup.Address1 != "" {
+		log.Fatal("No street provided.")
+	}
+
+	return this.lookup
+}
+func (this *Inputs) assembleLookupFromQueryString(values url.Values) {
+	this.lookup.Country = values.Get("street")
+	this.lookup.Language = street.Language(values.Get("language"))
+	this.lookup.Organization = values.Get("organization")
+	this.lookup.Freeform = values.Get("freeform")
+	this.lookup.Address1 = values.Get("address1")
+	this.lookup.Address2 = values.Get("address2")
+	this.lookup.Address3 = values.Get("address3")
+	this.lookup.Address4 = values.Get("address4")
+	this.lookup.Locality = values.Get("locality")
+	this.lookup.AdministrativeArea = values.Get("administrative_area")
+	this.lookup.PostalCode = values.Get("postal_code")
+	this.lookup.Geocode = values.Get("geocode") == "true"
+}
+
+func (this *Inputs) assembleLookupFromFlags() {
+	this.lookup.Country = this.country
+	this.lookup.Language = street.Language(this.language)
+	this.lookup.Organization = this.organization
+	this.lookup.Freeform = this.freeform
+	this.lookup.Address1 = this.address1
+	this.lookup.Address2 = this.address2
+	this.lookup.Address3 = this.address3
+	this.lookup.Address4 = this.address4
+	this.lookup.Locality = this.locality
+	this.lookup.AdministrativeArea = this.administrativeArea
+	this.lookup.PostalCode = this.postalCode
+	this.lookup.Geocode = this.geocode
+}
